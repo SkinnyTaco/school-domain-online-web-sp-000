6163f2083ddaa3b3eba3335c68f563268f6460d2
Daniel Dawson <dannyldawson@gmail.com>
2018-12-06 02:43:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+  
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.map do |grade, student_array|
+      student_array.sort
+    end
+    @roster
+  end
+end
